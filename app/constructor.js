8ae81a3b5205feaa6ca78e1155b1ec518605ee23
xiaoyuan <481704983@qq.com>
2015-12-23 00:37:09 +0800

    bug fixed

--- app/constructor.js
+++ app/constructor.js
@@ -1,22 +1,20 @@
- var obj = {
-    Slider : function () {
-        var what = document.createElement('LI');;
-        this.item = what;
+ var Slider = {
+    ele : function () {
+        this.item = document.createElement('LI');
         this.img = function (url) {
-            item.style.background = 'no-repeat center/100% url(' + url + ')';
+            this.item.style.background = 'no-repeat center/100% url(' + url + ')';
 
         };
         this.className = function (className) {
-            item.className = className;
+            this.item.className = className;
         };
 
     }
 };
- obj.Slider.prototype.ele = function () {
+ Slider.ele.prototype.init = function () {
      this.item.style.position = 'absolute';
      this.item.style.listStyle = 'none';
      this.item.style.width = '100%';
      this.item.style.height = '100%';
-     return item;
  };
- module.exports = obj;
+ module.exports = Slider;
--- app/init.js
+++ app/init.js
@@ -1,5 +1,4 @@
-var c = require('./constructor.js');
-console.log(c);
+var Slider = require('./constructor.js');
 
 function createSlider(parentNode, sliders, height, width) {
     var ul = document.createElement('UL');
@@ -12,31 +11,13 @@ function createSlider(parentNode, sliders, height, width) {
 
     for(var i = 0; i<sliders.length; i++){
         var j = i;
-        var slider = new c.Slider();
+        var slider = new Slider.ele();
+        slider.init();
         ul.appendChild(slider.item);
         slider.img( sliders[j] );
         slider.className(j);
     }
 }
-
-//var Slider = function() {
-//    var item = document.createElement('LI');
-//    this.img = function(url) {
-//        item.style.background = 'no-repeat center/100% url('+ url + ')';
-//
-//    };
-//    this.className = function(className){
-//      item.className = className;
-//    };
-//    Slider.prototype.ele = function(){
-//        item.style.position = 'absolute';
-//        item.style.listStyle = 'none';
-//        item.style.width = '100%';
-//        item.style.height = '100%';
-//        return item;
-//    };
-//};
-
 
 //test
 var sliders = [
