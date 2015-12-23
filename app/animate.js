9b71d31eafae266a519dbca742e3705f4d364632
xiaoyuan <481704983@qq.com>
2015-12-23 17:49:40 +0800

    animate.js

---
+++ app/animate.js
@@ -0,0 +1,82 @@
+var animate = {
+    opacity : function(slider,time) {
+        for (var i = slider.length; i > 1; i--) {
+            var j = i - 1;
+            slider[j].item.style.opacity = 0;
+        }
+
+        function In(ele) {
+            ele.style.opacity = 0;
+            function fadeIn() {
+                ele.style.opacity = parseFloat(ele.style.opacity) + 0.1;
+                var thisOpacity = ele.style.opacity;
+                if (thisOpacity >= 1) {
+                    ele.style.opacity = 1;
+                    clearInterval(run);
+                }
+            }
+
+            var run = setInterval(fadeIn, 100);
+        }
+
+        function Out(ele, nextEle) {
+            ele.style.opacity = 1;
+            function fadeOut() {
+                ele.style.opacity = parseFloat(ele.style.opacity) - 0.1;
+                var thisOpacity = ele.style.opacity;
+                if (thisOpacity <= 0) {
+                    ele.style.opacity = 0;
+                    clearInterval(run);
+                    In(nextEle);
+                }
+            }
+
+            var run = setInterval(fadeOut, 100)
+        }
+
+
+
+        var k = 0;
+
+        function start(next) {
+            var thisEle = slider[k].item;
+            var nextEle;
+            if(next == true) {
+                if (k == 0) {
+                    nextEle = slider[slider.length-1].item;
+                    Out(thisEle, nextEle);
+                    k = slider.length-1;
+                    return k;
+                } else {
+                    nextEle = slider[k-1].item;
+                    Out(thisEle, nextEle);
+                    k --;
+                    return k;
+                }
+            }else{
+                if (k < slider.length - 1) {
+                    nextEle = slider[k + 1].item;
+                    Out(thisEle, nextEle);
+                    k++;
+                    return k;
+                } else if (k = slider.length - 1) {
+                    nextEle = slider[0].item;
+                    Out(thisEle, nextEle);
+                    k = 0;
+                    return k;
+                }
+            }
+        }
+
+        var autoRun = setInterval(start, time);
+
+
+        var body = document.getElementsByTagName('body')[0];
+        body.addEventListener('click', function(){
+            clearInterval(autoRun);
+            start(1);
+            autoRun = setInterval(start, time);
+        })
+    }
+};
+module.exports = animate;
