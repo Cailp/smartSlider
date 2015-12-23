8088a8ba082958b783feb5599824a949838a8cb5
xiaoyuan <481704983@qq.com>
2015-12-23 14:59:42 +0800

    clear

--- app/init.js
+++ app/init.js
@@ -1,12 +1,9 @@
 var Slider = require('./constructor.js');
 
 var slider = {
-    //function createSlider(parentNode, sliders)
     createSlider: function(parentNode,sliders) {
         var ul = document.createElement('UL');
         parentNode.appendChild(ul);
-    //ul.style.height = height + 'px';
-    //ul.style.width = width + 'px' || '100%';
         ul.style.position = 'relative';
         ul.style.padding = '0';
         ul.style.margin = '0';
@@ -22,7 +19,6 @@ var slider = {
         return ul;
     },
 
-//function sliderResize(slider, breakPoints) {
     sliderResize: function(slider,breakPoints){
         var windowWidth = document.body.clientWidth;
         if (windowWidth < breakPoints.width[0]) {
@@ -39,24 +35,3 @@ var slider = {
 
 };
 module.exports = slider;
-
-
-////test
-//var sliders = [
-//    './imgs/slider01.jpg',
-//    './imgs/slider02.jpg',
-//    './imgs/slider03.jpg',
-//    './imgs/slider04.jpg',
-//    './imgs/slider05.jpg'
-//];
-//var body = document.getElementsByTagName('body')[0];
-//var testSlider = createSlider(body,sliders);
-//function toResize(){
-//    var breakPoints = {
-//        width : [320,480,768,960,1920],
-//        height: [160,330,425,474,500]
-//    };
-//    sliderResize(testSlider,breakPoints);
-//};
-//toResize();
-//window.addEventListener('resize',toResize);
