90632b62460eadad646e6be43c44b0941a16da6d
xiaoyuan <481704983@qq.com>
2015-12-24 15:34:25 +0800

    fix bug

--- app.js
+++ app.js
@@ -11,10 +11,9 @@ var sliders = [
 ];
 
 var body = document.getElementsByTagName('body')[0];
-
+var testSlider = init.createSlider(body, sliders);
 
 function toResize(){
-    var testSlider = init.createSlider(body, sliders);
     var breakPoints = {
         width : [320,480,768,960,1920],
         height: [160,330,425,474,500]
